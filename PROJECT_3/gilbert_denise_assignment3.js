5fb4b47cb817eb788591f1b452a85bc10e97d525
Denise Gilbert <dmg763@fullsail.edu>
2012-10-06 07:36:50 -0500

    Added Project 3 Files, a Global Variable, and a Procedure

---
+++ PROJECT_3/gilbert_denise_assignment3.js
@@ -0,0 +1,44 @@
+/* Denise M. Gilbert
+ * SDI 1210
+ * Project 3
+ * October 11, 2012
+ * Continuation of Christmas Story*/
+
+// Global Variables
+
+var toDoList = [
+    "Go Shopping",
+    "Gather Recipies",
+    "Wrap Presents",
+    "Get Tree",
+    "Address Christmas Cards",
+    "Make Stockings"
+];
+
+// Procedure
+
+var getToDoList = function (toDoList) {
+    if (toDoList.length !== 0) {
+	    console.log("I have several things left on my to do list.  I better get started!");
+    } else {
+	    console.log("I have completed everything I need to prepare for the holiday.");
+    };
+};
+
+// Boolean Function
+
+// Array Function
+
+// Number Function
+
+var getRecipes = function () {
+    
+};
+
+// String Function
+
+// Object Function
+
+// Returned Values to Output
+
+getToDoList(toDoList);
